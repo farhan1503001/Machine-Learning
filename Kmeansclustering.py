bb58ec41994ce171f3969ed49f9a655329037df8
Mohd. Farhan Israk Soumik <61477104+farhan1503001@users.noreply.github.com>
2021-02-12 03:06:38 +0600

    Add files via upload

---
+++ Kmeansclustering.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  4 02:42:16 2019
+
+@author: ASUS
+"""
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+dataset=pd.read_csv("Mall_Customers.csv")
+x=dataset
